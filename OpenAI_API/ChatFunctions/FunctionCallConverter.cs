15a061e893600c5af4bf6e0e29e8efb65040efd2
Jordan Hans <jordan.hans@ymail.com>
2023-07-14 09:05:29 -0500

    Revert "Update OpenAI_API/ChatFunctions/FunctionCallConverter.cs"
    This reverts commit bba1f36f26322134b087f48aa1a2c37dfe5e1d06.

--- OpenAI_API/ChatFunctions/FunctionCallConverter.cs
+++ OpenAI_API/ChatFunctions/FunctionCallConverter.cs
@@ -5,7 +5,6 @@ namespace OpenAI_API.ChatFunctions
     internal class FunctionCallConverter : JsonConverter
     {
         public FunctionCallConverter() : base() { }
-        public FunctionCallConverter() : base() { }
         public override bool CanConvert(Type objectType)
         {
             return (objectType == typeof(FunctionCall));
