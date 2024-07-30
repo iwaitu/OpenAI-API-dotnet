76446ad807a2bce72e912cbdd8259ddb28c0cf16
iwaitu <iwaitucn@gmail.com>
2024-07-30 19:08:32 +0800

    fix for llama3.1 70b awq 4bit

--- OpenAI_API/Chat/Conversation.cs
+++ OpenAI_API/Chat/Conversation.cs
@@ -399,7 +399,7 @@ namespace OpenAI_API.Chat
                     string deltaContent = delta.Content;
 					buffer_msg+= string.IsNullOrEmpty(deltaContent) ? "" : deltaContent;
                     //llama 3 70b 微调后，遇到function call 时，会先输出"```\n"
-                    if (!string.IsNullOrEmpty(deltaContent) && !buffer_msg.StartsWith("```\n") && !buffer_msg.StartsWith("```\nAction") && !buffer_msg.StartsWith(" Action"))
+                    if (!string.IsNullOrEmpty(deltaContent) && !buffer_msg.StartsWith("``") && !buffer_msg.StartsWith("```\n") && !buffer_msg.StartsWith("```\nAction") && !buffer_msg.StartsWith(" Action"))
                     {
                         responseStringBuilder.Append(deltaContent);
                         yield return deltaContent;
@@ -434,10 +434,10 @@ namespace OpenAI_API.Chat
                 }
             }
 
-            if (responseRole != null && responseStringBuilder.Length > 0)
-            {
-                AppendMessage(responseRole, responseStringBuilder.ToString());
-            }
+            //if (responseRole != null && responseStringBuilder.Length > 0)
+            //{
+            //    AppendMessage(responseRole, responseStringBuilder.ToString());
+            //}
         }
 
         public async IAsyncEnumerable<string> StreamResponseEnumerableFromPhi3ChatbotAsync()
